756c60c4fc5efe84a06eb78c99a67ddb901f1d32
shellyln <shellyln@users.noreply.github.com>
2018-02-11 22:47:46 +0900

    Add python lambda childprocess interface.

---
+++ redagate_lambda.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import json
+import os
+import subprocess
+import sys
+
+
+class AppInternalServerErrorException(Exception):
+    pass
+
+
+def call(command=None, event=None, timeout=None):
+    event_ = event
+    if not isinstance(event_, str):
+        event_ = json.dumps(event_, separators=(',', ':'))
+    with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE) as proc:
+        proc.stdin.write(event_.encode('UTF-8'))
+        try:
+            proc_status = -1
+            (output, err) = proc.communicate(timeout=timeout)
+            proc_status = proc.wait(timeout=0)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            (output, err) = proc.communicate()
+        if proc_status != 0:
+            raise AppInternalServerErrorException()
+        elif err is not None:
+            raise AppInternalServerErrorException()
+        return output
+
