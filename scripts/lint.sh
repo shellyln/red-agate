325f7bd4f159a80b948e401d8159f19cf533b65b
shellyln <shellyln@users.noreply.github.com>
2018-06-17 12:23:51 +0900

    Update CI settings.

---
+++ scripts/lint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+trap 'echo Error at Line $LINENO "$@"' ERR
+
+packages="./red-agate-util ./red-agate-svg-canvas ./red-agate-math ./red-agate ./red-agate-barcode ./_debug_app"
+
+cd ./packages
+
+for p in $packages; do
+    cd $p
+    npm run lint
+    cd ..
+done
+
+echo Done!!
