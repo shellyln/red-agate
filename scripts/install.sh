c60dc3940de4be64b265e6fa29fc328c22d642c2
shellyln <shellyln@users.noreply.github.com>
2018-09-09 05:46:20 +0900

    Edit build scripts.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 set -eu
 trap 'echo Error at Line $LINENO "$@"' ERR
 
-packages="./red-agate-util ./red-agate-svg-canvas ./red-agate-math ./red-agate ./red-agate-barcode ./red-agate-react-host ./_debug_app"
+packages="./red-agate-util ./red-agate-svg-canvas ./red-agate-math ./red-agate ./red-agate-barcode ./red-agate-react-host ./_debug_app ./_lib-dist"
 
 cd ./packages
 
