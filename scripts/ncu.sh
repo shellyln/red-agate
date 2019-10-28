32790017ef1578e705eac74b02059f05a765dc1c
shellyln <shellyln@users.noreply.github.com>
2019-10-28 22:04:19 +0900

    Update dependencies

---
+++ scripts/ncu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+trap 'echo Error at Line $LINENO "$@"' ERR
+
+packages="./red-agate-util ./red-agate-svg-canvas ./red-agate-math ./red-agate ./red-agate-barcode ./red-agate-react-host ./_debug_app ./_lib-dist"
+
+cd ./packages
+
+for p in $packages; do
+    cd $p
+    npm run ncu
+    cd ..
+done
+
+echo Done!!
